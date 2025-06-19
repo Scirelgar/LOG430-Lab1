699a4683aa6942763e06a3fda244ff0624c62903
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2025-06-18 20:12:41 -0400

    Finally something that works

--- src/model/product.py
+++ src/model/product.py
@@ -1,9 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer, Float
-
-
-class Base(DeclarativeBase):
-    pass
+from model.declarative_base import Base
 
 
 class Product(Base):
